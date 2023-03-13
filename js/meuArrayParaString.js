50de80c3fd693cb3b7d56da8531fb94df9315613
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-03-13 13:49:20 -0300

    Exemplo de como transformar um array em string.

---
+++ js/meuArrayParaString.js
@@ -0,0 +1,10 @@
+/* Declarando o array ; */
+let meuArray0 = [1,2,3,4,5,'a','b','c','d','e'];
+
+/* Transformando em String */
+let minhaString1 =  `${meuArray0}` ;
+let minhaString2 = meuArray0.toString();
+
+/* Transformando novamente em um array */
+let meuArray1 = minhaString1.split(',');
+let meuArray2 = mihaString2.split(',');
