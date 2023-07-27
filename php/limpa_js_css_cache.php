8b92e782daf802f8987f420fbb8c5b763e7baad7
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-07-27 13:51:22 -0300

    Melhoria limpa_js_css_cache.php limpando cache conforme arquivo js foi alterado.

--- php/limpa_js_css_cache.php
+++ php/limpa_js_css_cache.php
@@ -1,2 +1,5 @@
-<!-- ?version=timestamp; -->
+<!-- ?version=timestamp;  (Nunca guarda cache no js)-->
 <script src="./js/seuScriptJS.js?version=<?php echo time() ; ?>"></script>
+
+<!-- ?version=ultimaModificacaoFeitaNesseArquivoJs; (Sempre guarda cache, mas caso seja alterado o arquivo js no server perde o cache automáticamente) -->
+<script src="./js/seuScriptJS.js?version=<?= filemtime('./js/seuScriptJS.js') ;?>"></script>
