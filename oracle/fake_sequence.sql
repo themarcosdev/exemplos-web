4a1735434ac396fcbaf554d25d2da2cdbc93dab7
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-08-23 17:32:19 -0300

    Update fake_sequence.sql

--- oracle/fake_sequence.sql
+++ oracle/fake_sequence.sql
@@ -1,2 +1,15 @@
 select nvl (MAX(tab.colum_id), 0 ) + 1 as res
   from owner.db_tab tab
+
+-- FUNCTION : 
+CREATE OR REPLACE FUNCTION ret_sequence(p_coluna IN VARCHAR2, p_tabela IN VARCHAR2)
+  RETURN NUMBER
+IS
+  v_max_value NUMBER;
+BEGIN
+  -- Executa a consulta para obter o maior valor da coluna na tabela
+  EXECUTE IMMEDIATE 'SELECT NVL(MAX(' || p_coluna || '), 0) + 1 FROM ' || p_tabela INTO v_max_value;
+
+  -- Retorna o valor obtido
+  RETURN v_max_value;
+END;
